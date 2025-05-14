ac49c1b4f309e175dbbe423520d6e1231478f64f
jcesarmobile <jcesarmobile@gmail.com>
2025-05-14 15:23:41 +0200

    fmt

--- android/src/main/java/com/mycompany/capacitor/tap/jacking/CapacitorTapJackingPlugin.java
+++ android/src/main/java/com/mycompany/capacitor/tap/jacking/CapacitorTapJackingPlugin.java
@@ -23,23 +23,19 @@ public class CapacitorTapJackingPlugin extends Plugin {
     public void preventOverlays(PluginCall call) {
         getBridge()
             .getActivity()
-            .runOnUiThread(
-                    () -> {
-                        implementation.preventOverlays();
-                        call.resolve();
-                    }
-            );
+            .runOnUiThread(() -> {
+                implementation.preventOverlays();
+                call.resolve();
+            });
     }
 
     @PluginMethod
     public void enableOverlays(PluginCall call) {
         getBridge()
             .getActivity()
-            .runOnUiThread(
-                    () -> {
-                        implementation.enableOverlays();
-                        call.resolve();
-                    }
-            );
+            .runOnUiThread(() -> {
+                implementation.enableOverlays();
+                call.resolve();
+            });
     }
 }
